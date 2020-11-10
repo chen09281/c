4b79d9477d9265c462edfbae98b8aac963d5923f
chen09281 <67772169+chen09281@users.noreply.github.com>
2020-11-10 14:06:21 +0800

    Create a.c

---
+++ 三个数的和输出到屏幕/a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int number1=12;
+	int number2=34;
+	int number3=56;
+	int c;
+	c=number1+number2+number3;
+	printf("%d + %d + %d = %d",number1,number2,number3,c);
+	return 0;
+}
